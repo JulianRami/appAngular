03f8d0883792169ce5ea478161cb0794c33be7b8
Julian <julian.ramirez04@uptc.edu.co>
2023-05-04 11:48:55 -0500

    version inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ApiResponse {
+  name: string;
+  fecha: string;
+  time: string;
+  autor: string;
+  cantidad: string;
+  suma: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appAngular';
+export class AppComponent implements OnInit{
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  name: string;
+  fecha: string;
+  time: string;
+  autor: string;
+  cantidad: string;
+  suma: string;
+
+  constructor(private http: HttpClient) {
+    this.name = "";
+    this.fecha = "";
+    this.time = "";
+    this.autor = "";
+    this.cantidad= "";
+    this.suma = "";
+   }
+  ngOnInit(): void {
+    this.getData();
+  }
+  
+  getData() {
+    this.http.get<ApiResponse>('http://192.168.1.18:8001/').subscribe(data => {
+      this.autor = data.autor;
+      this.cantidad = data.cantidad;
+      this.suma = data.suma;
+    });
+      this.http.get<ApiResponse>('http://192.168.1.18:8080/').subscribe(data => {
+      this.autor = data.autor;
+      this.name = data.name;
+      this.fecha = data.fecha;
+      this.time = data.time;
+    });
+    
+  }
+  
 }
